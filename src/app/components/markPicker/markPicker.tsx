8bdc36a083f724acb8c689d96e98dd1c328c0e9f
agacia1805 <agacia1805@gmail.com>
2023-12-15 16:35:40 +0100

    (feature) create a function determining a winner of the game

--- src/app/components/markPicker/markPicker.tsx
+++ src/app/components/markPicker/markPicker.tsx
@@ -1,7 +1,22 @@
+import { Button } from '../button';
+import { FiX } from 'react-icons/fi';
+import { FiCircle } from 'react-icons/fi';
+import { FaXmark } from 'react-icons/fa6';
+
 export const MarkPicker = () => {
   return (
-    <div>
-        <h1>Pick your mark</h1>
-    </div>
+    <section className=''>
+      <h1 className='text-center text-xl font-semibold uppercase'>
+        Pick your mark
+      </h1>
+      <div className='flex items-center justify-center '>
+        <Button className='w-full px-16'>
+          <FaXmark size='42' />
+        </Button>
+        <Button className='w-full px-16'>
+          <FiCircle size='42' />
+        </Button>
+      </div>
+    </section>
   );
 };
