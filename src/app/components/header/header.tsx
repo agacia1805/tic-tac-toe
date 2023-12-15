98ddb3f3931146b6221827a420af984957743168
agacia1805 <agacia1805@gmail.com>
2023-12-15 16:35:09 +0100

    (chore) update header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,10 +4,10 @@ import { FaRegCircle } from 'react-icons/fa';
 
 export const Header = () => {
   return (
-    <header>
-    <Link  href="/" className='flex justify-center items-center'>
-      <FaXmark size="52" className="glitter-border"/>
-        <FaRegCircle size="39" />
+    <header className='mx-auto grid w-full scroll-m-[20vh] grid-cols-12 overflow-x-clip p-5 lg:px-10 lg:px-10'>
+      <Link href='/' className='flex items-center justify-center'>
+        <FaXmark size='52' />
+        <FaRegCircle size='39' />
       </Link>
     </header>
   );
