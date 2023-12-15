14a65389ae0d550fef685e4155f77ec85289c636
agacia1805 <agacia1805@gmail.com>
2023-12-15 16:34:49 +0100

    (chore) update app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Homepage } from './slices';
-import { Header } from './components';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center gap-16 p-24'>
-      <Header />
+    <main className='flex min-h-screen flex-col items-center gap-16 '>
       <Homepage />
     </main>
   );
