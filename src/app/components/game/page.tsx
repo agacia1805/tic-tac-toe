241e7f7465723ad8ea28ad26ecb3ac13cbdb0f2b
agacia1805 <agacia1805@gmail.com>
2023-12-19 23:42:27 +0100

    (chore) update game page

---
+++ src/app/components/game/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { useState } from 'react';
+import { Board } from '../board';
+
+export default function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  };
+
+  const hasEnoughMarks = (array: string[]): boolean => {
+    return array.filter((str) => str !== null).length > 4;
+  };
+
+  type SquareValue = 'X' | 'O' | null;
+  type BoardState = SquareValue[];
+
+  const determineWinner = (board: BoardState): SquareValue => {
+    if (!hasEnoughMarks(arrays)) return null;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  };
+  console.log(squares);
+
+  return <Board squares={squares} />;
+}
--- src/app/components/gameBoard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function GameBoard() {
-  return <div className='flex'></div>;
-}
