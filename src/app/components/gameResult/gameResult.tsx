1e62e083d52633f579d79b6cd5220cf76be75950
agacia1805 <agacia1805@gmail.com>
2024-01-05 15:33:13 +0000

    (feat) create game resul component

---
+++ src/app/components/gameResult/gameResult.tsx
@@ -0,0 +1,6 @@
+import { Button } from '../button';
+import { GameResult as GameResultType} from '@/app/types';
+
+export const GameResult = ({ gameResult }: { gameResult: GameResultType} ) => {
+  return <div className=''></div>;
+};
